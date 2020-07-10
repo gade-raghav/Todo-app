4bbc1e4543a7fa2956f07bf029309667bb015018
Raghav Gade <gaderaghav5@gmail.com>
2020-07-10 10:50:27 +0530

    commit-1: Initialized rep for to-do app

---
+++ to-do/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ragade/work/Python-Stand/django-/to-do/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ to-do/start.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cd ./todo
+cat requirements.txt
